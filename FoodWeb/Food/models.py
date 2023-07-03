600fac2ea793209f958ed0a64b9211e4e1a74224
Aayush3014 <ayushak3008@gmail.com>
2023-07-03 11:44:44 +0530

    Added str method in models.py for showing item by name in Shell

--- FoodWeb/Food/models.py
+++ FoodWeb/Food/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+
+    def __str__(self):  # This method is used to show item by its name in the Query Set.
+        return self.item_name
+
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    
