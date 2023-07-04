1c589867f19a6ba0d4cde53e76d9c8ede2ebc61d
Aayush3014 <ayushak3008@gmail.com>
2023-07-04 10:17:39 +0530

    Reading Data From Django

--- FoodWeb/Food/views.py
+++ FoodWeb/Food/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Item
 # Create your views here.
 
 def index(request):
-    return render(request, 'Food/index.html')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
