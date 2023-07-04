96ee23f68b6cf11c7348ad7818d32764717ca96f
Aayush3014 <ayushak3008@gmail.com>
2023-07-04 10:29:55 +0530

    Adding Templates in Food and load it in the views.py

--- FoodWeb/Food/views.py
+++ FoodWeb/Food/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
+
 # Create your views here.
 
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('Food/index.html')
+    context = {}
+
+    return HttpResponse(template.render(context,request))
