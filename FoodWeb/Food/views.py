2bb18609375d7ddd3419097de845dad81c93ff6d
Aayush3014 <ayushak3008@gmail.com>
2023-07-04 10:42:34 +0530

    usig render instead of template loader

--- FoodWeb/Food/views.py
+++ FoodWeb/Food/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
-from django.template import loader
+
 
 # Create your views here.
 
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('Food/index.html')
     context = {
         'item_list':item_list
     }
 
-    return HttpResponse(template.render(context,request))
+    return render(request,'Food/index.html',context)
