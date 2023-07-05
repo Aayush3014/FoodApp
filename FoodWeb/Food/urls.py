d62975cfa545c6bada708e8d987dc3b2b71118ed
Aayush3014 <ayushak3008@gmail.com>
2023-07-05 10:39:19 +0530

    Configuring multiple app and same url name problem

--- FoodWeb/Food/urls.py
+++ FoodWeb/Food/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'Food'
+
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('<int:item_id>/',views.detail, name = "deatil"),
-    
+    path('',views.index, name = 'index'),    # Name Parameter is used for removing the hardcoded urls
+    path('<int:item_id>/',views.detail, name = "detail"),  # It is used by removing url from href and user 
+                                                           # name given for the particular url
 ]
