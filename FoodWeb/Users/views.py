32018a2a4d8596c430be6fb6b6516df479eea0cd
Aayush3014 <ayushak3008@gmail.com>
2023-07-14 14:10:36 +0530

    Adding Email Field

--- FoodWeb/Users/views.py
+++ FoodWeb/Users/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm  # We will use RegisterForm which we have created for adding an email field
+                                #instead of using UserCreationForm which is default provided by Django. 
 
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":  # Corrected condition
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            
+
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome {username}, your account is created.')
+            messages.success(
+                request, f'Welcome {username}, your account is created.')
             return redirect('Food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'Users/register.html', {'form': form})
 
-    return HttpResponse("Invalid form submission")   # This line is added to handle fallback for invalid form submission.
+    # This line is added to handle fallback for invalid form submission.
+    return HttpResponse("Invalid form submission")
 
     """This HttpResponse is added above to handle this error
     raise ValueError( ValueError: The view Users.views.register didn't return an HttpResponse object. 
