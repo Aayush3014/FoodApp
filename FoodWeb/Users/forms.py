ae15d1bc861a099df929e951f49d2509887a4ce1
Aayush3014 <ayushak3008@gmail.com>
2023-07-14 18:39:33 +0530

    Adding Email Field

--- FoodWeb/Users/forms.py
+++ FoodWeb/Users/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
+# We have created this register Form by inheriting UserCreationForm.
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        
