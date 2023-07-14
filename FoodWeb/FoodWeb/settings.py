47c2dde9c2529bbb2631c46b8bad1e6c6129b7da
Aayush3014 <ayushak3008@gmail.com>
2023-07-14 19:05:08 +0530

    Adding Login Feature

--- FoodWeb/FoodWeb/settings.py
+++ FoodWeb/FoodWeb/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Default login redirect page if no template is given to the user.
+LOGIN_REDIRECT_URL = 'Food:index'
--- FoodWeb/FoodWeb/urls.py
+++ FoodWeb/FoodWeb/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Users import views as user_views
+from django.contrib.auth import views as authentication_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Food.urls')),
     path('register/',user_views.register,name='register'),
+    path('login/', authentication_view.LoginView.as_view(template_name='Users/login.html'), name = "login"),
+    path('logout/',authentication_view.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
 ]
